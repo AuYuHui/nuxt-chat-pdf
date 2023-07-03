a0c388e0dd6233d5e7adf24d0f2e27439b0b9bbf
cyh <2419068625@qq.com>
2023-07-03 23:59:27 +0800

    fix: 网页数据

--- server/api/web_page.post.ts
+++ server/api/web_page.post.ts
@@ -1,18 +1,36 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { v4 as uuidv4 } from 'uuid'
 import { WebPageLoader } from '~/utils/loaders/web-page'
 
 export default defineEventHandler(async (event) => {
   const { url } = await readBody<{ url: string }>(event)
   const web_page = new WebPageLoader()
 
-  const content = await web_page.load_data(url.trim())
+  const datas = await web_page.load_data(url.trim())
   const textSplitter = new RecursiveCharacterTextSplitter({
     chunkSize: 500,
     chunkOverlap: 0,
     keepSeparator: false,
   })
-
-  console.log('content', content)
+  const documents = []
+  const ids = []
+  const metadatas = []
+  for (const data of datas) {
+    const content = data.pageContent
+    const meta_data = data.metadata
+    if (content) {
+      const chunks = await textSplitter.splitText(content)
+      for (const chunk of chunks) {
+        const chunk_id = uuidv4()
+        ids.push(chunk_id)
+        documents.push(chunk)
+        metadatas.push(meta_data)
+      }
+      console.log('documents', documents)
+      console.log('ids', ids)
+      console.log('metadatas', metadatas)
+    }
+  }
 
   return 'Hello webpage'
 })
