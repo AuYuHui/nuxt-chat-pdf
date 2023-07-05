ab97507e4f2e6ab5ab554c6b752dba251d0ab584
cyh <2419068625@qq.com>
2023-07-05 11:44:31 +0800

    feat: google 搜索接入

--- server/api/chat_agent.post.ts
+++ server/api/chat_agent.post.ts
@@ -1,36 +1,57 @@
-import { ChatOpenAI } from 'langchain/chat_models/openai'
 import { SerpAPI } from 'langchain/tools'
-import { initializeAgentExecutorWithOptions } from 'langchain/agents'
+import { AgentExecutor, ZeroShotAgent } from 'langchain/agents'
 import { Calculator } from 'langchain/tools/calculator'
+import { OpenAI } from 'langchain/llms/openai'
+import { LLMChain } from 'langchain/chains'
 
 export default defineEventHandler(async (event) => {
-  const { prompt } = await readBody<{ prompt: string }>(event)
-  const { GOOGLE_CUSTOM_SEARCH_ENGINE_ID, GOOGLE_CUSTOM_SEARCH_API_KEY }
+  const { prompt: input } = await readBody<{ prompt: string }>(event)
+  const { SerpAPI_KEY }
 		= process.env
-  const model = new ChatOpenAI({ temperature: 0 }, {
+  const model = new OpenAI({
+    temperature: 0.9,
+  }, {
     basePath: process.env.OPENAI_PROXY_URL,
   })
   const tools = [
-    new SerpAPI('', {
+    new SerpAPI(SerpAPI_KEY, {
       location: 'China',
       hl: 'zh-cn',
       gl: 'cn',
     }),
     new Calculator(),
   ]
+  const prefix = 'Answer the following questions as best you can. And answer according to the language of the user\'s question. You have access to the following tools:\n\nsearch: a search engine. useful for when you need to answer questions about current events. input should be a search query.\ncalculator:'
+  const suffix = `Begin! And answer according to the language of the user's question."
 
-  const executor = await initializeAgentExecutorWithOptions(tools, model, {
-    agentType: 'zero-shot-react-description',
-  })
-  console.log('Loaded agent.')
+Question: {input}
+{agent_scratchpad}`
+  const createPromptArgs = {
+    suffix,
+    prefix,
+    inputVariables: ['input', 'agent_scratchpad'],
+  }
+  try {
+    const prompt = ZeroShotAgent.createPrompt(tools, createPromptArgs)
+    const llmChain = new LLMChain({ llm: model, prompt })
+    const agent = new ZeroShotAgent({
+      llmChain,
+      allowedTools: ['search', 'calculator'],
+    })
+    const agentExecutor = AgentExecutor.fromAgentAndTools({ agent, tools })
+    console.log('Loaded agent.')
 
-  const input = encodeURIComponent('蔡徐坤最近做了什么事情?')
+    console.log(`Executing with input "${input}"...`)
 
-  console.log(`Executing with input "${input}"...`)
+    const result = await agentExecutor.call({ input })
+    console.log('result', result)
 
-  const result = await executor.call({ input })
+    console.log(`Got output ${result.output}`)
 
-  console.log(`Got output ${result.output}`)
-
-  return 'Hello chat_agent'
+    return result.output
+  }
+  catch (error) {
+    console.log(error)
+    throw new Error('出错了')
+  }
 })
